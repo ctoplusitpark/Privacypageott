e30d1365bc2a9348043a1a7d4664000ffd8ac404
Akshathpitbp <akshath.intern@worldwideplus.in>
2024-08-19 23:08:30 +0530

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <Navbar />
+    // <Navbar />
+    <Footer/>
   );
 }
 
---
+++ src/Components/Footer.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+
+      <div className="footer-div1">
+       <p className="footer-appName">Indrajala Movie Makers LLP. </p>
+       <p className="footer-c"> © 2024 Copyrights  All Rights Reserved</p>
+        <p className="footer-about">
+        Indrajala Movie Makers LLP takes pride in providing a wide range of
+        fantasy content to our users. We are committed to maintaining the
+        highest standards of quality and ensuring a safe and enjoyable
+        experience for all our viewers.
+        </p>
+      </div>
+
+    <div className="footer-links1">
+    <li className="footer-links">Account Deletion</li>
+    <li className="footer-links">Age Restrictions</li>
+    <li className="footer-links">Broken Links</li>
+    <li className="footer-links">Cancellation Policy</li>
+    <li className="footer-links">Child Protection</li>
+    </div>
+
+    <div className="footer-links2">
+    <li className="footer-links">Contend Removal</li>
+    <li className="footer-links">Cookie Policy</li>
+    <li className="footer-links">Copyright Policy</li>
+    <li className="footer-links">User Conduct</li>
+    <li className="footer-links">Contacts</li>
+    </div>
+
+    <div className="footer-links3">
+    <li className="footer-links">Child Protection</li>
+    <li className="footer-links">Contend Removal</li>
+    <li className="footer-links">Cookie Policy</li>
+    <li className="footer-links">Copyright Policy</li>
+    <li className="footer-links">Contacts</li> 
+    <li className="footer-links">User Conduct</li>
+    </div>
+
+    
+    </div>
+  );
+};
+
+export default Footer;
